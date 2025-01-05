caa2ff1ddb5e49fa00244602754b4ceab08eac2f
Ankit Kumar Singh <ankittchauhan937@gmail.com.com>
2025-01-05 10:00:27 +0530

    Bitwise Operators Number Systems

---
+++ 18 Bitwise Operator/src/com/ankit/FindUnique.java
@@ -0,0 +1,16 @@
+package com.ankit;
+
+public class FindUnique {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 5, 6, 3, 5, 2};
+        System.out.println(ans(arr));
+    }
+    private static int ans(int[] arr){
+        int unique = 0;
+        for (int n : arr){
+            unique ^= n;
+        }
+        return unique;
+    }
+}
+
